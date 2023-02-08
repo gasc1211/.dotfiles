a91e4c4e77d4e4e0c9687bdef6ca3d819689cd5a
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2023-02-08 12:42:40 -0600

    Added dashtodock mods script

---
+++ scripts/dashtodock.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.shell.extensions.dash-to-dock background-color '#1A1B26';
+gsettings set org.gnome.shell.extensions.dash-to-dock opacity 0.75;
+gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style 'DASHES';
+
