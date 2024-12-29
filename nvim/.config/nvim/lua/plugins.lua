f95f43ac3c98d103e04aef84647e367a4fd7d425
GASC1211 <48170476+Gasc1211@users.noreply.github.com>
2024-12-29 11:15:34 -0600

    Add folke/snacks.nvim

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,4 +1,25 @@
 local plugins = {
+  -- Snacks (Homescreen )
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
   -- Colorscheme and Transparency
   {
     "folke/tokyonight.nvim",
@@ -9,6 +30,15 @@ local plugins = {
       vim.cmd([[colorscheme tokyonight]])
     end,
   },
+  -- Icons
+  { 
+    'echasnovski/mini.icons', 
+    version = '*',
+    config = function()
+      require("mini.icons").setup()
+    end
+  },
+  "nvim-tree/nvim-web-devicons",
   {
     'tribela/transparent.nvim',
     event = 'VimEnter',
